efcb6e55961201da8de913c7ad772968c538352c
Yuta Koshizawa <koher@koherent.org>
2017-11-10 16:36:39 +0900

    Support Carthage for installation

---
+++ Sources/EasyDL/EasyDL.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for EasyDL.
+FOUNDATION_EXPORT double EasyDLVersionNumber;
+
+//! Project version string for EasyDL.
+FOUNDATION_EXPORT const unsigned char EasyDLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EasyDL/PublicHeader.h>
+
+
